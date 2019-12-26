852c032e2252111bb407976dae76364fcf01b338
Jorge Isla <jorge.isla@ug.uchile.cl>
2019-12-25 22:13:11 -0300

    Added some description

--- modelo_sportingCristal/models.py
+++ modelo_sportingCristal/models.py
@@ -1,39 +1,57 @@
 from django.db import models
 
-# Create your models here.
 
+# Modelo de datos inicial
+
+'''Clase stadium
+donde se incluye su nombre, ciudad, latitud y longitud'''
 class stadium(models.Model):
     name = models.CharField()
     city = models.CharField()
     latitude = models.IntegerField()
     longitude = models.IntegerField()
 
+'''Clase referee
+se utiliza para almacenar los nombres de los arbitros'''
 class referee(models.Model):
     name = models.CharField()
 
+'''Clase team
+se utiliza para almacenar los distintos equipos con su nombre, shortName y una imagen. 
+'''
 class team(models.Model):
     name =  models.CharField()
     shortName = models.CharField()
     image = models.CharField()
 
+'''Clase championship
+se utilizara para almacenar los distintos torneos con su nombre y un contexto'''
 class championship(models.Model):
     context = models.CharField()
     name = models.CharField
 
+'''Clase actions
+no se realmente como se utilizará
+'''
 class actions(models.Model):
     type = models.CharField()
     icon = models.ImageField()
     url = models.CharField()
 
+'''Clase state
+se utilizará para almacenar los distintos estados de un partido con un nombre y un tipo'''
 class state(models.Model):
     name = models.CharField()
     type = models.CharField()
 
+'''Clase meta'''
 class meta(models.Model):
     own_side = models.CharField()
     is_own_match = models.IntegerField()
 
-
+'''Clase match
+se utilizara para almacenar todo lo relacionado a un partido
+'''
 class match(models.Model):
     context = models.CharField()
     championship = models.ForeignKey(championship)
